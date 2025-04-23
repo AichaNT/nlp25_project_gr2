1ffcd7baf904291ec791687728daaf6c46293105
Norazouhtane <nozo@itu.dk>
2025-04-23 12:20:13 +0200

    .

--- data_aug_code/ME_ORG.py
+++ data_aug_code/ME_ORG.py
@@ -1,12 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("../data_aug_sources/middle_eastern_organisations.csv", sep = ";", skiprows = 1)
-
 def add_organisation(organisation):
     tokens = organisation.split()  
     tags = ['B-ORG'] + ['I-ORG'] * (len(tokens) - 1)
     return {"tokens": tokens, "tags": tags}
 
-unique_orgs = df["org"].drop_duplicates()
-ME_ORG = [add_organisation(org) for org in unique_orgs]
 
