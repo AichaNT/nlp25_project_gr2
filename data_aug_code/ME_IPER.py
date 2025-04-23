a8a2af888e717564c270fddc8eba62e8b1866a0e
Norazouhtane <nozo@itu.dk>
2025-04-23 12:59:04 +0200

    .

--- data_aug_code/ME_IPER.py
+++ data_aug_code/ME_IPER.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('data_aug_sources/KDBGIVE.tsv', sep='\t', skiprows = 82 )
-I_PER = df["Roman"].tolist()
+#df = pd.read_csv('data_aug_sources/KDBGIVE.tsv', sep='\t', skiprows = 82 )
+#I_PER = df["Roman"].tolist()
 
 def extract_last_names(name_str):
     lines = name_str.split("\n")
