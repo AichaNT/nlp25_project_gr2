3ac49504ba2d38a4c264b0c66346ac197b09856a
Norazouhtane <nozo@itu.dk>
2025-05-07 10:59:50 +0200

    hey karima

--- scripts/train_aug.py
+++ scripts/train_aug.py
@@ -66,7 +66,7 @@ import torch
 from evaluate import load 
 
 # path to the data files
-path_me_dev = "data/me_data/middle_eastern_dev.tsv" 
+path_me_dev = "data/me_data/middle_eastern_dev.tsv"  
 
 # saving model name
 model_name = "vesteinn/DanskBERT"
@@ -78,36 +78,18 @@ label2id, id2label = mapping(path_train)
 num_labels = len(label2id)
 
 # reading in the data
-train_data = read_tsv_file(path_train, label2id=label2id)
 dev_data = read_tsv_file(path_dev, label2id=label2id)
-test_data = read_tsv_file(path_test, label2id=label2id)
 me_dev_data = read_tsv_file(path_me_dev, label2id=label2id)
 
 # convert to huggingface format
-train_dataset = Dataset.from_list(train_data)
 dev_dataset = Dataset.from_list(dev_data)
-test_dataset = Dataset.from_list(test_data)
 me_dev_dataset = Dataset.from_list(me_dev_data)
-
-# tokenize train dataset and align labels with subword tokens
-tokenized_train_dataset = train_dataset.map(
-    tokenize_and_align_labels,
-    batched = True,
-    remove_columns=train_dataset.column_names
-)
 
 # tokenize and align dev dataset
 tokenized_dev_dataset = dev_dataset.map(
     tokenize_and_align_labels,
     batched=True,
     remove_columns=dev_dataset.column_names
-)
-
-# tokenize and align test dataset
-tokenized_test_dataset = test_dataset.map(
-    tokenize_and_align_labels,
-    batched=True,
-    remove_columns=test_dataset.column_names
 )
 
 # tokenize and align ME dev dataset
@@ -145,32 +127,10 @@ model.to(device) # move model to device
 # load the seqeval metric 
 metric = load("seqeval") 
 
-# function to compute metrics
-def compute_metrics(predictions):
-    '''
-    This function computes evaluation metrics.
-
-    Parameters: 
-    - predictions
-
-    Returns:
-    - a dictionary with precision, recall, f1 and accuracy
-    '''
-    true_labels, pred_labels = pred2label(predictions, id2label)
-
-    results = metric.compute(predictions = pred_labels, references = true_labels)
-
-    return {
-        "Precision": results["overall_precision"],
-        "Recall": results["overall_recall"],
-        "F1-score": results["overall_f1"],
-        "Accuracy": results["overall_accuracy"]
-    }
-
 # define the training arguments
 args = TrainingArguments(
     output_dir = "output_trainer", 
-    eval_strategy = 'epoch', 
+    eval_strategy = 'no', 
     save_strategy = "no",
     learning_rate = 2e-5,
     per_device_train_batch_size = 2,
@@ -180,15 +140,23 @@ args = TrainingArguments(
     remove_unused_columns=False
 )
 
-# define parameters for trainer
-trainer = Trainer(
-    model = model,
-    args = args,
-    train_dataset = tokenized_train_dataset,
-    eval_dataset = tokenized_dev_dataset,
-    compute_metrics = compute_metrics,
-    data_collator = data_collator
-)
+# loop for training sets
+for train_data in augmented_datasets:
+    train_dataset = Dataset.from_list(train_data) # convert to huggingface format
 
-# train the model
-trainer.train()
+    tokenized_train_dataset = train_dataset.map( # tokenize train dataset and align labels with subword tokens
+        tokenize_and_align_labels,
+        batched = True,
+        remove_columns=train_dataset.column_names
+    )
+
+    # define parameters for trainer
+    trainer = Trainer(
+        model = model,
+        args = args,
+        train_dataset = tokenized_train_dataset,
+        data_collator = data_collator
+    )
+
+    # train the model
+    trainer.train()
